ff91d1da59b0cdf54f099e7ae791c2bacf8927ef
omarloren <omarloren@gmail.com>
2012-07-31 11:06:12 -0500

    docs and unused imports

--- deliverer/GraficaHandler.java
+++ deliverer/GraficaHandler.java
@@ -9,64 +9,71 @@ import oms.Grafica.Graphic;
  * @author omar
  */
 public class GraficaHandler {
-    
+
     public static ArrayList<Graphic> graficas = new ArrayList();
-    static ArrayList<ArrayList> ordPool = new ArrayList();
     
     /**
      * Si queremos añadir una nueva grafica acá es donde.
+     *
      * @param symbol
-     * @param periodo 
+     * @param periodo
      */
-    public void addGrafica(String symbol, int periodo){
-        //Podriamos poner aquí algunas opciones mas como pasar el archivo log
+    public void addGrafica(String symbol, int periodo) {
+        //Podriamos poner aquí algunas opciones mas como pasar el archivo log.
         graficas.add(new Graphic(symbol, periodo, this.getId(symbol)));
-        runGrafica(graficas.size()-1);
-        
+        runGrafica(graficas.size() - 1);
+
     }
-    
-    private void runGrafica(int index){
+
+    private void runGrafica(int index) {
         graficas.get(index).start();
     }
-    
-    public void orderEntry(){
-        
+
+    public static void orderAccept(String id, String ordid) {
+        for (int i = 0; i < graficas.size(); i++) {
+            if(graficas.get(i).getID().equals(id)){
+                                
+            }
+            
+        }
     }
-    
+
     /**
      * Este método genera un id único para cada gráfica.
+     *
      * @return id único.
      */
-    private String getId(String id){
+    private String getId(String id) {
         //TODO buscar donde poner esta cosa, o talvez no...
-        StringBuffer str = new StringBuffer(id+"-");
+        StringBuffer str = new StringBuffer(id + "-");
         Random r = new Random();
         Random r2 = new Random();
         int n = 25; // 65-90 codigo ASCII.
-        
-        
-        for (int j=0; j<4; j++){
+
+
+        for (int j = 0; j < 4; j++) {
             int i = r.nextInt() % n;
-            if ( (65 + i <= 90) && (65 + i >= 65)){
-                str.append ((char) (65 + i));  
+            if ((65 + i <= 90) && (65 + i >= 65)) {
+                str.append((char) (65 + i));
+            } else {
+                j--;//<---------|| ¡CUIDADO ESTO ES UNA CHARRADA!
             }
-            else j--;//<---------|| ¡CUIDADO ESTO ES UNA CHARRADA!
         }
         return str.toString();
     }
+    
     /*
      * Obtenemos el ask de una determinada grafica.
      */
-    public Double getAsk(String idgraf){
+    public static Double getAsk(String idgraf) {
         return getGraf(idgraf).getAsk();
-        
     }
-    
-    public static Graphic getGraf(String id){
+
+    public static Graphic getGraf(String id) {
         Graphic temp = null;
-        for(int i=0; i<=graficas.size();i++){
-            if(graficas.get(i).getID().equals(id)){
-                temp= graficas.get(i);
+        for (int i = 0; i <= graficas.size(); i++) {
+            if (graficas.get(i).getID().equals(id)) {
+                temp = graficas.get(i);
                 break;
             }
         }
