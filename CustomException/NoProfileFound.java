3ba4a5b98d0e455b80f4c17ad0dc37c3f370d6b6
omarloren <omarloren@gmail.com>
2013-02-19 10:18:57 -0600

    rname/add Exceptions

--- CustomException/NoProfileFound.java
+++ CustomException/NoProfileFound.java
@@ -2,18 +2,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package CustomException;
+package oms.CustomException;
 
 /**
  *
  * @author omar
  */
-public class NoProfileFoundException extends Exception{
-    public NoProfileFoundException(){
+public class NoProfileFound extends Exception{
+    public NoProfileFound(){
         super("No se encontro el perfil o está vacío, por favor crea un un nuevo"
                 + " perfil o añade una gráfica, por lo pronto usaremos el deafult");
     }   
-    public NoProfileFoundException(String perfil){
+    public NoProfileFound(String perfil){
         super("No se encontro el perfil \""+ perfil +"\" o está vacío, por favor crea un un nuevo"
                 + " perfil o añade una gráfica, por lo pronto usaremos el deafult");
     }
---
+++ CustomException/OrdenNotFound.java
@@ -0,0 +1,13 @@
+package oms.CustomException;
+
+/**
+ * @author omar
+ * Si al buscar una Orden en el array en donde se almacenan estoas, no se encuentra
+ * lanzamos este tipo de excepción.
+ */
+public class OrdenNotFound extends Exception{
+    
+    public OrdenNotFound(String id){
+        super("####No se pudo encontrar una orden para "+ id +", esta orden no existe! ");
+    }
+}
